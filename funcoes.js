bc22e4e92e38fe6c26a2441a6423a5da6c8980f1
MARIA CECILIA PEREIRA JARDIM <25132596@CFP127ALUNO.LOCAL>
2025-08-20 11:24:11 -0300

    exemplos feitos em aula com professor ensinando como usar o onclick. Estilizei o css.

---
+++ funcoes.js
@@ -0,0 +1,8 @@
+function calcularSoma(){
+    let v1 = parseInt(document.getElementById("n1").value)
+    let v2 = parseInt(document.getElementById("n2").value)
+
+    let total = v1 + v2
+
+    document.getElementById("resultado").innerHTML = total
+}
